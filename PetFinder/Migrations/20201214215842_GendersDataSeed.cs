e492435f5bcdad82129cb27edab831bddeae8d15
Germandf <germandefrancesco@hotmail.com>
2020-12-14 19:36:41 -0300

    Agrego DataSeed para la tabla gender en PetFinderContext y agrego el migration GendersDataSeed

---
+++ PetFinder/Migrations/20201214215842_GendersDataSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetFinder.Migrations
+{
+    public partial class GendersDataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Macho" });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Hembra" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genders",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
