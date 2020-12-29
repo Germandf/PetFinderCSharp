046ee609591ec60bcb79cded4a74ccd27d7931af
Germandf <germandefrancesco@hotmail.com>
2020-12-28 22:54:34 -0300

    Corrijo model Comment.cs que le faltaba la declaración de la columna Rate

--- PetFinder/Models/Comment.cs
+++ PetFinder/Models/Comment.cs
@@ -9,8 +9,9 @@ namespace PetFinder.Models
     public partial class Comment
     {
         public int Id { get; set; }
-        public string Message { get; set; }
         public string UserId { get; set; }
         public int PetId { get; set; }
+        public string Message { get; set; }
+        public int Rate { get; set; }
     }
 }
