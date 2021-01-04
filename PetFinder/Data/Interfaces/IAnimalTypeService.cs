436d12b46a619c222cf08a528d9723b235c18b71
Germandf <germandefrancesco@hotmail.com>
2021-01-04 20:08:06 -0300

    Documento inferfaz AnimalType

--- PetFinder/Data/Interfaces/IAnimalTypeService.cs
+++ PetFinder/Data/Interfaces/IAnimalTypeService.cs
@@ -9,13 +9,68 @@ namespace PetFinder.Data
 {
     interface IAnimalTypeService
     {
+        /// <summary>
+        /// Gets all AnimalTypes
+        /// </summary>
+        /// <returns>
+        /// An IEnumerable of type AnimalType
+        /// </returns>
         Task<IEnumerable<AnimalType>> GetAll();
+
+        /// <summary>
+        /// Gets a specific AnimalType by its Id
+        /// </summary>
+        /// <returns>
+        /// An AnimalType object
+        /// </returns>
         Task<AnimalType> Get(int Id);
+
+        /// <summary>
+        /// Inserts an AnimalType
+        /// </summary>
+        /// <returns>
+        /// A bool that indicates if it was successfull or not
+        /// </returns>
         Task<bool> Insert(AnimalType animalType);
+
+        /// <summary>
+        /// Updates an AnimalType
+        /// </summary>
+        /// <returns>
+        /// A bool that indicates if it was successfull or not
+        /// </returns>
         Task<bool> Update(AnimalType animalType);
-        Task<bool> Delete(int id );
+
+        /// <summary>
+        /// Deletes an AnimalType
+        /// </summary>
+        /// <returns>
+        /// A bool that indicates if it was successfull or not
+        /// </returns>
+        Task<bool> Delete(int id);
+
+        /// <summary>
+        /// Insert or Update an AnimalType depending on the case.
+        /// </summary>
+        /// <returns>
+        /// A GenericResult that indicates if it was successfull or not
+        /// </returns>
         Task<GenericResult> Save(AnimalType animalType);
+
+        /// <summary>
+        /// Check if a name is used by an already created AnimalType
+        /// </summary>
+        /// <returns>
+        /// A bool that indicates if it's being used or not
+        /// </returns>
         Task<bool> IsRepeated(string name);
+
+        /// <summary>
+        /// Check if exists a Pet that depends on a specific AnimalType
+        /// </summary>
+        /// <returns>
+        /// A bool that indicates if there is at least one
+        /// </returns>
         Task<bool> HasNoPetsAssociated(AnimalType animalType);
     }
 }
