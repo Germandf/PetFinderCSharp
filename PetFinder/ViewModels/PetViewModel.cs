86dfdc1b46964d8479b8ab581afd4c8f6eade639
Germandf <germandefrancesco@hotmail.com>
2021-01-14 12:16:08 -0300

    Creo PetViewModel

---
+++ PetFinder/ViewModels/PetViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PetFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinder.ViewModels
+{
+    public class PetViewModel
+    {
+        public string Name { get; set; }
+        public int AnimalTypeId { get; set; }
+        public int CityId { get; set; }
+        public int GenderId { get; set; }
+        public DateTime Date { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+
+        public Pet ConvertToPet()
+        {
+            var pet = new Pet();
+            pet.Name = Name;
+            pet.AnimalTypeId = AnimalTypeId;
+            pet.CityId = CityId;
+            pet.GenderId = GenderId;
+            pet.Date = Date;
+            pet.PhoneNumber = PhoneNumber;
+            pet.Description = Description;
+            return pet;
+        }
+    }
+}
