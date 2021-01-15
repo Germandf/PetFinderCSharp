5a062cbc46e58cb06aed4dc18e15eeabd1b8480c
Germandf <germandefrancesco@hotmail.com>
2021-01-15 13:20:43 -0300

    Soluciono errores en PetViewModel: ahora se puede editar correctamente

--- PetFinder/ViewModels/PetViewModel.cs
+++ PetFinder/ViewModels/PetViewModel.cs
@@ -8,17 +8,20 @@ namespace PetFinder.ViewModels
 {
     public class PetViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int AnimalTypeId { get; set; }
         public int CityId { get; set; }
         public int GenderId { get; set; }
         public DateTime Date { get; set; }
         public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
         public string Description { get; set; }
 
         public Pet ConvertToPet()
         {
             var pet = new Pet();
+            pet.Id = Id;
             pet.Name = Name;
             pet.AnimalTypeId = AnimalTypeId;
             pet.CityId = CityId;
@@ -27,6 +30,19 @@ namespace PetFinder.ViewModels
             pet.PhoneNumber = PhoneNumber;
             pet.Description = Description;
             return pet;
+        }
+
+        public void ConvertFromPet(Pet pet)
+        {
+            Id = pet.Id;
+            Name = pet.Name;
+            AnimalTypeId = pet.AnimalTypeId;
+            CityId = pet.CityId;
+            GenderId = pet.GenderId;
+            Date = pet.Date;
+            PhoneNumber = pet.PhoneNumber;
+            Photo = pet.Photo;
+            Description = pet.Description;
         }
     }
 }
