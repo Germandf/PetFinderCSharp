61f06611632cedf3039851a20102b26f1fe2e1b1
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-01-19 13:18:38 -0300

    Agrego CategoryViewModel

---
+++ PetFinder/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PetFinder.Models;
+
+namespace PetFinder.ViewModels
+{
+    public class CategoryViewModel<T> where T: CategoryBase, new()
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public T ConvertToCategory()
+        {
+            var animalType = new T {Id = Id, Name = Name};
+            return animalType;
+        }
+
+        public void ConvertFromCategory(T category)
+        {
+            Id = category.Id;
+            Name = category.Name;
+        }
+    }
+}
