6d1e4c64e7211901b887bf2efb9390db71b186a8
Germandf <germandefrancesco@hotmail.com>
2021-01-19 13:33:27 -0300

    Agrego interfaz ICommentApiService

---
+++ PetFinder/Data/ICommentApiService.cs
@@ -0,0 +1,19 @@
+﻿using PetFinder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PetFinder.Data
+{
+    public interface ICommentApiService
+    {
+        public Task<HttpResponseMessage> CreateComment(CommentViewModel commentViewModel);
+
+        public Task<HttpResponseMessage> DeleteComment(CommentViewModel commentViewModel);
+
+        public Task<HttpResponseMessage> GetComments();
+
+        public Task<HttpResponseMessage> UpdateComment(CommentViewModel commentViewModel);
+    }
+}
